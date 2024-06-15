17784cae2729ee465012691da903443f7117e252
its-shivam1008 <tanil5960@gmail.com>
2024-06-15 14:45:15 +0530

    user Routes completed

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -35,6 +35,54 @@ router.post('/login', async(req,res) =>{
     }catch(err){
         res.status(500).json({error: "Internal server error"})
     }
+});
+
+router.get('/profile', jwtAuthMiddleware, async(req,res) =>{
+    try{
+        const userData = req.user;
+        const userId = userData.id;
+        const user = await User.findById(userId);
+        res.status(200).json({user});
+    }catch(err){
+        res.status(500).json({error:"Internal server error"});
+    }
+});
+
+router.put('/profile/:field', jwtAuthMiddleware, async(req,res)=>{
+    try{
+        const field = req.params.field;
+        const data = req.body;
+        const userId = req.user.id;
+        if(field=='address' || field=="contactNum" || field=="password"){
+
+            if(field=="password"){
+
+                const {currentPass, newPass} = req.body;
+
+                const user = await User.findById(userId);
+
+                if(!user || !(await user.comparePassword(currentPass))) 
+                    res.status(401).json({error:"Incorrect password"});
+        
+                user.password = newPass;
+                const response = await user.save();
+
+                res.status(200).json({response,mssg:"Updated "+field});
+
+            }else{
+                const response = await User.findByIdAndUpdate(userId, data, {
+                runValidators: true,
+                new:true
+                });
+
+                res.status(200).json({response,mssg:"Updated "+field});
+            }
+        }else{
+            res.status(403).json({response,mssg:"You are not allowed to update"+field});
+        }
+    }catch(err){
+        res.status(500).json({error:"Internal server error"});
+    }
 })
 
 module.exports = router;
--- jwt.js
+++ jwt.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 const jwtAuthMiddleware = function(req,res,next){
     const authorization = req.headers.authorization;
@@ -7,7 +8,7 @@ const jwtAuthMiddleware = function(req,res,next){
         const token = req.headers.authorization.split(" ")[1];
         if(!token) return res.status(401).json({error:"Unauthorized"});
         const decoded = jwt.verify(token, process.env.JWT_KEY);
-        req.data = decoded;
+        req.user = decoded;
         next();
     }catch(err){
         res.status(401).json({error:"Invalid token"});
