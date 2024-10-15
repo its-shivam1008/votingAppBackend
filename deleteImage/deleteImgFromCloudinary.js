645b453d2ea6519a2120c7d0e7f13f8ac2fa5eec
its-shivam1008 <tanil5960@gmail.com>
2024-10-16 00:36:46 +0530

    add deleteImage concept to replace the image from cloudinary

---
+++ deleteImage/deleteImgFromCloudinary.js
@@ -0,0 +1,33 @@
+
+export const deleteImageFromCloudinary = async (imgUrl) => {
+    try {
+      
+      const publicId = imgUrl.split('/').pop().split('.')[0];
+      const data =  {
+        public_id: publicId,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        timestamp: Math.floor(Date.now() / 1000),
+        signature: generateSignature(publicId, process.env.CLOUDINARY_API_SECRET) 
+      }
+  
+      const response = await axios.post(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/image/destroy`, 
+        data
+      );
+  
+      console.log('Delete response:', response.data);
+      return response.data;
+    } catch (error) {
+      console.error('Error deleting image from Cloudinary:', error);
+      throw error;
+    }
+  }
+  
+  
+  const generateSignature = (publicId, apiSecret) => {
+    const crypto = require('crypto');
+    const timestamp = Math.floor(Date.now() / 1000);
+    const signature = crypto.createHash('sha1')
+      .update(`public_id=${publicId}&timestamp=${timestamp}${apiSecret}`)
+      .digest('hex');
+    return signature;
+  };
