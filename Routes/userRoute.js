a544b9486f4f6d45426b1552e196cb287d405755
its-shivam1008 <tanil5960@gmail.com>
2024-10-16 22:43:07 +0530

    error field for login route

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -177,7 +177,7 @@ router.post("/login", async (req, res) => {
       res.status(200).json({ message: "Login success", success: true, token });
     }
   } catch (err) {
-    res.status(500).json({ message: "Internal server error", success: false });
+    res.status(500).json({ message: "Internal server error",error:err, success: false });
   }
 });
 
